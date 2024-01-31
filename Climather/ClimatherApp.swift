e35ce9c8fa8b30a20edb9bde2191a09764d39105
Minisock <goldenkitamar@gmail.com>
2024-02-01 01:42:22 +0530

    Initial Commit

---
+++ Climather/ClimatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimatherApp.swift
+//  Climather
+//
+//  Created by Amarjeet Kumar on 01/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ClimatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Climather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Climather
+//
+//  Created by Amarjeet Kumar on 01/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
